a55e2a279aefc12a913ab524667acacc98957ff6
Mahadi9664 <momahadi9664@gmail.com>
2025-01-02 03:55:32 +0600

    ran

--- plates.py
+++ plates.py
@@ -9,27 +9,22 @@ def main():
 def is_valid(s):
     length = len(s)
 
-    # Check length
     if length < 2 or length > 6:
         return False
 
-    # Check first two characters are letters
-    if not s[:2].isalpha():
+    if not s[0:2].isalpha():
         return False
 
-    # Check for only alphanumeric characters
     if not s.isalnum():
         return False
 
     found_digit = False
     for char in s:
         if char.isdigit():
-            # Check for leading '0' in numbers
             if not found_digit and char == '0':
                 return False
             found_digit = True
         elif found_digit:
-            # Return False if letter appears after number
             return False
         
     return True
