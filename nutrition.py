ace17ec8b3a1e966d639912107c1a4415eb519e6
Mahadi9664 <momahadi9664@gmail.com>
2025-01-02 04:10:30 +0600

    nutrition.py

---
+++ nutrition.py
@@ -0,0 +1,25 @@
+nutrition = {
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwano": 90,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "watermelon": 80
+}   
+
+x = input("Item: ").lower()
+
+if x in nutrition:
+    print("Calories: ", nutrition[x])
